13bca30f4944b06cfad7bdc5e82c11c5dc5207cd
jeayeon <47214341+jeayeon@users.noreply.github.com>
2026-08-06 21:10:03 +0900

    배경음악 출력 크기 상향

--- src/audio/AdaptiveMusic.ts
+++ src/audio/AdaptiveMusic.ts
@@ -156,7 +156,7 @@ export class AdaptiveMusic {
   }
 
   private getMusicGain(): number {
-    return Math.max(0.0001, (this.mode === 'boss' ? 0.35 : 0.27) * this.musicVolume);
+    return Math.max(0.0001, (this.mode === 'boss' ? 0.7 : 0.54) * this.musicVolume);
   }
 
   private getEffectsGain(): number {
